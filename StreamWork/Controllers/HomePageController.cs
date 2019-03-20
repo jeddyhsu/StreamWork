db15b663dc98ab3e396a44cd5cbcbcf003111ee9
rarun321 <rithvikarun24@gmail.com>
2019-03-20 15:44:43 -0700

    Changes

--- StreamWork/Controllers/HomePageController.cs
+++ StreamWork/Controllers/HomePageController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
 
 namespace StreamWork.Controllers
 {
@@ -6,6 +7,7 @@ namespace StreamWork.Controllers
     {
         public IActionResult Index()
         {
+           
             return View();
         }
     }
