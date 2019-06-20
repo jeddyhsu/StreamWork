79a16707b00e69199dcca06b106c1bdc4f962efc
sage <ssaguUUUU@gmail.com>
2019-06-20 12:16:14 -0700

    How To Stream Page
    Added page and edited navbar

--- StreamWork/Controllers/HomeController.cs
+++ StreamWork/Controllers/HomeController.cs
@@ -80,6 +80,11 @@ namespace StreamWork.Controllers
             return View();
         }
 
+        public IActionResult HowToStream()
+        {
+            return View();
+        }
+
         [HttpGet]
         public async Task<IActionResult> ProfileView(string Tutor,[FromServices] IOptionsSnapshot<StorageConfig> storageConfig)
         {
