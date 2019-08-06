04f850feb026dab156601eec3e28ac37808a8dab
sage <ssaguUUUU@gmail.com>
2019-08-06 11:15:10 -0700

    IPN
    Doesn't work yet. Should be published for testing.

---
+++ StreamWork/DataModels/Donation.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StreamWork.Framework;
+
+namespace StreamWork.Models {
+    public class Donation : IStorageBase<Donation> {
+        [Key]
+        public string Id { get; set; }
+        public string Student { get; set; }
+        public string Tutor { get; set; }
+        public string Val { get; set; }
+        public string TimeSent { get; set; }
+        public string Verified { get; set; }
+        public string Error { get; set; }
+
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+
+        public virtual void Configure (EntityTypeBuilder<Donation> builder) { }
+    }
+}
