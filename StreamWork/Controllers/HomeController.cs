af883293887794de7e82d435a4157e62fe8f79a8
sage <ssaguUUUU@gmail.com>
2019-08-10 17:49:02 -0700

    IPN SQL & Tests
    Now Donations are properly logged.

--- StreamWork/Controllers/HomeController.cs
+++ StreamWork/Controllers/HomeController.cs
@@ -11,6 +11,11 @@ using Microsoft.Extensions.Options;
 using StreamWork.ViewModels;
 using StreamWork.DataModels;
 
+// THE FOLLOWING WILL BE DELETED AFTER TESTING
+using System.IO;
+using System.Net;
+using System.Text;
+
 namespace StreamWork.Controllers
 {
     public class HomeController : Controller
@@ -91,6 +96,23 @@ namespace StreamWork.Controllers
                 userArchivedVideos = await DataStore.GetListAsync<UserArchivedStreams>(_connectionString, storageConfig.Value, "UserArchivedVideos", new List<string> { Tutor }),
                 userLogins = await DataStore.GetListAsync<UserLogin>(_connectionString, storageConfig.Value, "PaticularSignedUpUsers", new List<string> { Tutor })
             };
+            return View(profile);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Donate(string Tutor, [FromServices] IOptionsSnapshot<StorageConfig> storageConfig) {
+            ProfileTutorViewModel profile = new ProfileTutorViewModel {
+                userLogins = await DataStore.GetListAsync<UserLogin>(_connectionString, storageConfig.Value, "PaticularSignedUpUsers", new List<string> { Tutor })
+            };
+
+            Donation donation = new Donation {
+                Id = Guid.NewGuid().ToString(),
+                Student = HttpContext.Session.GetString("UserProfile"),
+                Tutor = profile.userLogins[0].Username,
+                TimeSent = DateTime.Now.ToString(),
+            };
+            await DataStore.SaveAsync(_connectionString, storageConfig.Value, new Dictionary<string, object> { { "Id", donation.Id } }, donation);
+
             return View(profile);
         }
 
@@ -224,6 +246,119 @@ namespace StreamWork.Controllers
         public IActionResult Login()
         {
             return View();
+        }
+
+        //
+        //
+        // EVERYTHING BEYOND THIS POINT WILL BE REMOVED AFTER TESTING
+        //
+        //
+
+        private IOptionsSnapshot<StorageConfig> storageConfig;
+
+        private class IPNContext {
+            public HttpRequest IPNRequest { get; set; }
+
+            public string RequestBody { get; set; }
+
+            public string Verification { get; set; } = String.Empty;
+        }
+
+        public IActionResult TestDonate ([FromServices] IOptionsSnapshot<StorageConfig> storageConfig) {
+            return Receive(storageConfig);
+        }
+
+        // Should run when an IPN is recieved
+        [HttpPost]
+        public IActionResult Receive (IOptionsSnapshot<StorageConfig> _storageConfig) {
+            storageConfig = _storageConfig;
+
+            IPNContext ipnContext = new IPNContext {
+                IPNRequest = Request
+            };
+
+            using (StreamReader reader = new StreamReader(ipnContext.IPNRequest.Body, Encoding.ASCII)) {
+                ipnContext.RequestBody = reader.ReadToEnd();
+            }
+
+            //Store the IPN received from PayPal
+            Donation donation = LogRequest(ipnContext);
+
+            //Fire and forget verification task
+            Task.Run(() => VerifyTask(ipnContext, donation));
+
+            //Reply back a 200 code
+            return Ok();
+        }
+
+        private async Task VerifyTask (IPNContext ipnContext, Donation donation) {
+            try {
+                var verificationRequest = WebRequest.Create("https://www.sandbox.paypal.com/cgi-bin/webscr");
+
+                //Set values for the verification request
+                verificationRequest.Method = "POST";
+                verificationRequest.ContentType = "application/x-www-form-urlencoded";
+
+                //Add cmd=_notify-validate to the payload
+                string strRequest = "cmd=_notify-validate&" + ipnContext.RequestBody;
+                verificationRequest.ContentLength = strRequest.Length;
+
+                //Attach payload to the verification request
+                using (StreamWriter writer = new StreamWriter(verificationRequest.GetRequestStream(), Encoding.ASCII)) {
+                    writer.Write(strRequest);
+                }
+
+                //Send the request to PayPal and get the response
+                using (StreamReader reader = new StreamReader(verificationRequest.GetResponse().GetResponseStream())) {
+                    ipnContext.Verification = reader.ReadToEnd();
+                }
+            }
+            catch (Exception exception) {
+                //Capture exception for manual investigation
+                donation.Error += "...Failed to verify IPN task: " + exception.Message;
+            }
+
+            await ProcessVerificationResponse(ipnContext, donation);
+        }
+
+        // Should find the table entry created when payment was made, set verified to FALSE, and return it, and optionally discard incomplete donations.
+        private Donation LogRequest (IPNContext ipnContext) {
+            // Persist the request values into a database or temporary data store
+
+            return new Donation {
+                Id = Guid.NewGuid().ToString(), // Generated earlier
+                Student = "student123", // Earlier
+                Tutor = "streamtutor123", // Earlier
+                Val = "20.00",
+                TimeSent = DateTime.Now.ToString(), // Earlier
+                Verified = "FALSE",
+                Error = ""
+            };
+        }
+
+        // Should update table entry on payment validity
+        private async Task ProcessVerificationResponse (IPNContext ipnContext, Donation donation) {
+            if (ipnContext.Verification.Equals("VERIFIED")) {
+                if (donation.Error.Equals("")) {
+                    donation.Verified = "TRUE";
+                }
+
+                // check that Payment_status=Completed
+                // check that Txn_id has not been previously processed
+                // check that Receiver_email is your Primary PayPal email
+                // check that Payment_amount/Payment_currency are correct
+                // process payment
+            }
+            else if (ipnContext.Verification.Equals("INVALID")) {
+                donation.Error += "...IPN could not be verified because it was found to be INVALID";
+            }
+            else {
+                donation.Error += "...IPN could not be verified for unknown reasons (neither VALID nor INVALID)";
+            }
+
+            donation.Error += "...DEBUG NOTES...IPNRequest.Body = " + ipnContext.IPNRequest.Body + "...RequestBody = " + ipnContext.RequestBody + "...Verification = " + ipnContext.Verification;
+
+            await DataStore.SaveAsync(_connectionString, storageConfig.Value, new Dictionary<string, object> { { "Id", donation.Id } }, donation);
         }
     }
 }
