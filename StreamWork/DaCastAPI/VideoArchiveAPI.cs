37eb38409bde0366245c28cabf0c77b104dc1448
“rarun321CORRECT_EMAIL=“43283635+rarun321@users.noreply.github.com <>
2019-08-16 15:00:35 -0700

    DaCast Stream Integration

---
+++ StreamWork/DaCastAPI/VideoArchiveAPI.cs
@@ -0,0 +1,181 @@
+﻿namespace StreamWork.DaCastAPI
+{
+    using System;
+    using Newtonsoft.Json;
+
+    public partial class VideoArchiveAPI
+    {
+        [JsonProperty("totalCount")]
+        [JsonConverter(typeof(ParseStringConverter))]
+        public long TotalCount { get; set; }
+
+        [JsonProperty("data")]
+        public Datum[] Data { get; set; }
+
+        [JsonProperty("paging")]
+        public Paging Paging { get; set; }
+    }
+
+    public partial class Datum
+    {
+        [JsonProperty("abitrate")]
+        public long Abitrate { get; set; }
+
+        [JsonProperty("acodec")]
+        public string Acodec { get; set; }
+
+        [JsonProperty("ads")]
+        public object Ads { get; set; }
+
+        [JsonProperty("associated_packages")]
+        public string AssociatedPackages { get; set; }
+
+        [JsonProperty("category_id")]
+        public long CategoryId { get; set; }
+
+        [JsonProperty("container")]
+        public string Container { get; set; }
+
+        [JsonProperty("countries_id")]
+        public long CountriesId { get; set; }
+
+        [JsonProperty("creation_date")]
+        public DateTimeOffset CreationDate { get; set; }
+
+        [JsonProperty("custom_data")]
+        public object CustomData { get; set; }
+
+        [JsonProperty("noframe_security")]
+        public long NoframeSecurity { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("disk_usage")]
+        [JsonConverter(typeof(ParseStringConverter))]
+        public long DiskUsage { get; set; }
+
+        [JsonProperty("duration")]
+        public DateTimeOffset Duration { get; set; }
+
+        [JsonProperty("autoplay")]
+        public bool Autoplay { get; set; }
+
+        [JsonProperty("enable_coupon")]
+        public bool EnableCoupon { get; set; }
+
+        [JsonProperty("online")]
+        public bool Online { get; set; }
+
+        [JsonProperty("enable_payperview")]
+        public bool EnablePayperview { get; set; }
+
+        [JsonProperty("publish_on_dacast")]
+        public bool PublishOnDacast { get; set; }
+
+        [JsonProperty("enable_subscription")]
+        public bool EnableSubscription { get; set; }
+
+        [JsonProperty("external_video_page")]
+        public string ExternalVideoPage { get; set; }
+
+        [JsonProperty("filename")]
+        public string Filename { get; set; }
+
+        [JsonProperty("filesize")]
+        public long Filesize { get; set; }
+
+        [JsonProperty("google_analytics")]
+        public long GoogleAnalytics { get; set; }
+
+        [JsonProperty("group_id")]
+        public long GroupId { get; set; }
+
+        [JsonProperty("hds")]
+        public Uri Hds { get; set; }
+
+        [JsonProperty("hls")]
+        public string Hls { get; set; }
+
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("is_secured")]
+        public bool IsSecured { get; set; }
+
+        [JsonProperty("original_id")]
+        public long OriginalId { get; set; }
+
+        [JsonProperty("password")]
+        public object Password { get; set; }
+
+        [JsonProperty("pictures")]
+        public Pictures Pictures { get; set; }
+
+        [JsonProperty("player_height")]
+        public long PlayerHeight { get; set; }
+
+        [JsonProperty("player_width")]
+        public long PlayerWidth { get; set; }
+
+        [JsonProperty("player_size_id")]
+        public long PlayerSizeId { get; set; }
+
+        [JsonProperty("referers_id")]
+        public long ReferersId { get; set; }
+
+        [JsonProperty("save_date")]
+        public DateTimeOffset SaveDate { get; set; }
+
+        [JsonProperty("share_code")]
+        public ShareCode ShareCode { get; set; }
+
+        [JsonProperty("splashscreen_id")]
+        public long SplashscreenId { get; set; }
+
+        [JsonProperty("streamable")]
+        public long Streamable { get; set; }
+
+        [JsonProperty("subtitles")]
+        public object Subtitles { get; set; }
+
+        [JsonProperty("template_id")]
+        public long TemplateId { get; set; }
+
+        [JsonProperty("theme_id")]
+        public long ThemeId { get; set; }
+
+        [JsonProperty("thumbnail_id")]
+        public long ThumbnailId { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("vbitrate")]
+        public long Vbitrate { get; set; }
+
+        [JsonProperty("vcodec")]
+        public string Vcodec { get; set; }
+
+        [JsonProperty("video_height")]
+        public long VideoHeight { get; set; }
+
+        [JsonProperty("video_width")]
+        public long VideoWidth { get; set; }
+    }
+
+    public partial class Paging
+    {
+        [JsonProperty("self")]
+        public string Self { get; set; }
+
+        [JsonProperty("last")]
+        public string Last { get; set; }
+
+        [JsonProperty("next")]
+        public object Next { get; set; }
+
+        [JsonProperty("previous")]
+        public object Previous { get; set; }
+    }
+}
---
+++ StreamWork/Threads/ThreadClass.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using StreamWork.Config;
+using StreamWork.Core;
+using StreamWork.DataModels;
+using StreamWork.Models;
+
+namespace StreamWork.Threads
+{
+    public class ThreadClass
+    {
+        HelperFunctions helperFunctions;
+        IOptionsSnapshot<StorageConfig> storageConfig;
+        UserChannel userChannel;
+        string streamTitle;
+        string streamSubject;
+
+        public ThreadClass(IOptionsSnapshot<StorageConfig> storageConfig, UserChannel userChannel, string streamTitle, string streamSubject)
+        {
+            helperFunctions = new HelperFunctions();
+            this.userChannel = userChannel;
+            this.storageConfig = storageConfig;
+            this.streamTitle = streamTitle;
+            this.streamSubject = streamSubject;
+        }
+
+        public bool RunThread()
+        {
+            //This thread handles getting streamed videos to our archive DB
+            var cancellationToken = new CancellationToken();
+            Task.Factory.StartNew(async () =>
+            {
+                userChannel.StreamSubject = streamSubject;
+                userChannel.StreamTitle = streamTitle;
+                userChannel.StreamThumbnail = "default";
+                await DataStore.SaveAsync(helperFunctions._connectionString, storageConfig.Value, new Dictionary<string, object> { { "Id", userChannel.Id } }, userChannel);
+
+                while (true)
+                {
+                    await Task.Delay(10000, cancellationToken);
+
+                    var liveRecording = DataStore.CallRecordingAPI("https://api.dacast.com/v2/channel/" + userChannel.ChannelKey + "/recording/watch?apikey=135034_9d5e445816dfcd2a96ad&_format=JSON");
+                    if (liveRecording.RecordingStatus == "recording")
+                    {
+                        Console.WriteLine("Recording");
+                    }
+                    else
+                    {
+                        await StopStreamAndArchive();
+                        await ClearChannelStreamInfo();
+                        return true;
+                    }
+                }
+            }, TaskCreationOptions.LongRunning);
+            return false;
+        }
+
+        private async Task<bool> StopStreamAndArchive()
+        {
+            //stop stream and archvie video into database
+            var currentDate = DateTime.Now.ToString("ddd/MMM/d/yyyy").Replace('/', ' ');
+
+            //strict format!!!
+            var archivedVideo = DataStore.CallVideoArchiveAPI("https://api.dacast.com/v2/vod?apikey=135034_9d5e445816dfcd2a96ad&title=" + "(" + userChannel.ChannelKey + ")" + " - " + currentDate);
+
+            UserArchivedStreams archivedStream = new UserArchivedStreams
+            {
+                Id = Guid.NewGuid().ToString(),
+                Username = userChannel.Username,
+                StreamID = archivedVideo.Data[0].Id.ToString(),
+                StreamTitle = userChannel.StreamTitle,
+                StreamSubject = userChannel.StreamSubject,
+                StreamThumbnail = archivedVideo.Data[0].Pictures.Thumbnail[0].ToString(),
+            };
+            await DataStore.SaveAsync(helperFunctions._connectionString, storageConfig.Value, new Dictionary<string, object> { { "Id", archivedStream.Id } }, archivedStream);
+            return true;
+        }
+
+        private async Task ClearChannelStreamInfo()
+        {
+            try
+            {
+                userChannel.StreamTitle = null;
+                userChannel.StreamSubject = null;
+                userChannel.StreamThumbnail = null;
+                await DataStore.SaveAsync(helperFunctions._connectionString, storageConfig.Value, new Dictionary<string, object> { { "Id", userChannel.Id } }, userChannel);
+            }
+            catch (Microsoft.EntityFrameworkCore.DbUpdateConcurrencyException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+
+
+}
