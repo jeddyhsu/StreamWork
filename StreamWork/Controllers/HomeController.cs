bfe2a7259d91db02d3d35c12a490a8569f815158
sage <ssaguUUUU@gmail.com>
2019-08-26 12:07:33 -0700

    IPN Witchery
    I barely know how this works and I wrote it lol. PayPal casts a magic spell and somehow there's data in our database.

--- StreamWork/Controllers/HomeController.cs
+++ StreamWork/Controllers/HomeController.cs
@@ -22,6 +22,7 @@ namespace StreamWork.Controllers
     {
         HelperFunctions helperFunctions = new HelperFunctions();
 
+
         public IActionResult Index()
         {
             if (Request.Host.ToString() == "streamwork.live")
@@ -251,119 +252,6 @@ namespace StreamWork.Controllers
         public IActionResult Login()
         {
             return View();
-        }
-
-        //
-        //
-        // EVERYTHING BEYOND THIS POINT WILL BE REMOVED AFTER TESTING
-        //
-        //
-
-        private IOptionsSnapshot<StorageConfig> storageConfig;
-
-        private class IPNContext {
-            public HttpRequest IPNRequest { get; set; }
-
-            public string RequestBody { get; set; }
-
-            public string Verification { get; set; } = String.Empty;
-        }
-
-        public IActionResult TestDonate ([FromServices] IOptionsSnapshot<StorageConfig> storageConfig) {
-            return Receive(storageConfig);
-        }
-
-        // Should run when an IPN is recieved
-        [HttpPost]
-        public IActionResult Receive (IOptionsSnapshot<StorageConfig> _storageConfig) {
-            storageConfig = _storageConfig;
-
-            IPNContext ipnContext = new IPNContext {
-                IPNRequest = Request
-            };
-
-            using (StreamReader reader = new StreamReader(ipnContext.IPNRequest.Body, Encoding.ASCII)) {
-                ipnContext.RequestBody = reader.ReadToEnd();
-            }
-
-            //Store the IPN received from PayPal
-            Donation donation = LogRequest(ipnContext);
-
-            //Fire and forget verification task
-            Task.Run(() => VerifyTask(ipnContext, donation));
-
-            //Reply back a 200 code
-            return Ok();
-        }
-
-        private async Task VerifyTask (IPNContext ipnContext, Donation donation) {
-            try {
-                var verificationRequest = WebRequest.Create("https://www.sandbox.paypal.com/cgi-bin/webscr");
-
-                //Set values for the verification request
-                verificationRequest.Method = "POST";
-                verificationRequest.ContentType = "application/x-www-form-urlencoded";
-
-                //Add cmd=_notify-validate to the payload
-                string strRequest = "cmd=_notify-validate&" + ipnContext.RequestBody;
-                verificationRequest.ContentLength = strRequest.Length;
-
-                //Attach payload to the verification request
-                using (StreamWriter writer = new StreamWriter(verificationRequest.GetRequestStream(), Encoding.ASCII)) {
-                    writer.Write(strRequest);
-                }
-
-                //Send the request to PayPal and get the response
-                using (StreamReader reader = new StreamReader(verificationRequest.GetResponse().GetResponseStream())) {
-                    ipnContext.Verification = reader.ReadToEnd();
-                }
-            }
-            catch (Exception exception) {
-                //Capture exception for manual investigation
-                donation.Error += "...Failed to verify IPN task: " + exception.Message;
-            }
-
-            await ProcessVerificationResponse(ipnContext, donation);
-        }
-
-        // Should find the table entry created when payment was made, set verified to FALSE, and return it, and optionally discard incomplete donations.
-        private Donation LogRequest (IPNContext ipnContext) {
-            // Persist the request values into a database or temporary data store
-
-            return new Donation {
-                Id = Guid.NewGuid().ToString(), // Generated earlier
-                Student = "student123", // Earlier
-                Tutor = "streamtutor123", // Earlier
-                Val = "20.00",
-                TimeSent = DateTime.Now.ToString(), // Earlier
-                Verified = "FALSE",
-                Error = ""
-            };
-        }
-
-        // Should update table entry on payment validity
-        private async Task ProcessVerificationResponse (IPNContext ipnContext, Donation donation) {
-            if (ipnContext.Verification.Equals("VERIFIED")) {
-                if (donation.Error.Equals("")) {
-                    donation.Verified = "TRUE";
-                }
-
-                // check that Payment_status=Completed
-                // check that Txn_id has not been previously processed
-                // check that Receiver_email is your Primary PayPal email
-                // check that Payment_amount/Payment_currency are correct
-                // process payment
-            }
-            else if (ipnContext.Verification.Equals("INVALID")) {
-                donation.Error += "...IPN could not be verified because it was found to be INVALID";
-            }
-            else {
-                donation.Error += "...IPN could not be verified for unknown reasons (neither VALID nor INVALID)";
-            }
-
-            donation.Error += "...DEBUG NOTES...IPNRequest.Body = " + ipnContext.IPNRequest.Body + "...RequestBody = " + ipnContext.RequestBody + "...Verification = " + ipnContext.Verification;
-
-            await helperFunctions.SaveDonation(storageConfig, donation);
         }
     }
 }
