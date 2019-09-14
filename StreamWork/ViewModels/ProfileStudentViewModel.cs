60742d37f8e3b80e34422633f1ff716d3e872135
rarun321 <43283635+rarun321@users.noreply.github.com>
2019-09-13 22:28:15 -0700

    Updated Student Profile with asthetic changes like the tutor profile

---
+++ StreamWork/ViewModels/ProfileStudentViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using StreamWork.Models;
+
+namespace StreamWork.ViewModels
+{
+    public class ProfileStudentViewModel
+    {
+        public UserProfile userProfile { get; set; }
+        public List<UserLogin> userLogins { get; set; }
+    }
+}
