71d9963afa3dbea3e8ce785187b460b985a5545c
rarun321 <43283635+rarun321@users.noreply.github.com>
2019-09-25 20:36:50 -0700

    Updated Student

--- StreamWork/Controllers/StudentController.cs
+++ StreamWork/Controllers/StudentController.cs
@@ -13,24 +13,22 @@ namespace StreamWork.Controllers
 {
     public class StudentController : Controller
     {
+        HelperFunctions helperFunctions = new HelperFunctions();
+
         private readonly string _connectionString = "Server=tcp:streamwork.database.windows.net,1433;Initial Catalog=StreamWork;Persist Security Info=False;User ID=streamwork;Password=arizonastate1!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
         public async Task<IActionResult> ProfileStudent([FromServices] IOptionsSnapshot<StorageConfig> storageConfig)
         {
             var model = new UserProfile();
             var user = HttpContext.Session.GetString("UserProfile");
-            var getUserInfo = await DataStore.GetListAsync<UserLogin>(_connectionString, storageConfig.Value, "PaticularSignedUpUsers", new List<string> { user });
-            foreach (var u in getUserInfo)
-            {
-                var splitName = u.Name.Split(new char[] { '|' });
-                model.FirstName = splitName[0];
-                model.LastName = splitName[1];
-            }
+            var userProfile = await helperFunctions.GetUserProfile(storageConfig, "CurrentUser", user);
+            var splitName = userProfile.Name.Split(new char[] { '|' });
+            model.FirstName = splitName[0];
+            model.LastName = splitName[1];
 
             ProfileStudentViewModel viewModel = new ProfileStudentViewModel
             {
-                userProfile = model,
-                userLogins = await DataStore.GetListAsync<UserLogin>(_connectionString, storageConfig.Value, "PaticularSignedUpUsers", new List<string> { user })
+                userLogins = await helperFunctions.GetUserLogins(storageConfig, "CurrentUser", user)
             };
 
             return View(viewModel);
