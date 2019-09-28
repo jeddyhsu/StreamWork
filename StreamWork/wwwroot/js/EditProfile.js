57d75eefb88d1e16bf308b351f3eb14f709e6c24
rarun321 <43283635+rarun321@users.noreply.github.com>
2019-09-27 18:33:14 -0700

    Added Keep Logged In Features

---
+++ StreamWork/wwwroot/js/EditProfile.js
@@ -0,0 +1,69 @@
+﻿//Sends profile caption and paragraph to backend for saving
+    function RegisterProfilePhotoAndCaption() {
+        var formData = new FormData();
+        var profileCaption = $("#ProfileCaption").val()
+        var profileParagraph = $("#ProfileParagraph").val()
+        var userInfo = "";
+
+         if (profileCaption != "" && profileParagraph == "") {
+                    userInfo = profileCaption + "|" + "NA";
+                }
+                else if (profileCaption == "" && profileParagraph != "") {
+                    userInfo = "NA" + "|" + profileParagraph;
+                }
+                else if (profileCaption != "" && profileParagraph != "") {
+                    userInfo = profileCaption + "|" + profileParagraph;
+                }
+                else {
+                    userInfo = "NA|NA"
+                }
+              
+
+        var totalFiles = document.getElementById("uploadProfilePic").files.length;
+        if(totalFiles != 0){
+             formData.append(userInfo, document.getElementById("uploadProfilePic").files[0]);
+        }
+        else {
+
+            formData.append(userInfo, "No Profile Pic");
+        }
+       
+        $.ajax({
+            type: "POST",
+            url: '/Tutor/ProfileTutor',
+            data: formData,
+            dataType: 'json',
+            contentType: false,
+            processData: false,
+            success: function (data) {
+                if (data.message === "Success") {
+                    location.reload()
+                }
+            }
+        });
+    }
+
+ //Opens editing MODAL
+ function EditProfile() {
+        $('#editModal').modal('show')
+    }
+    
+//Reads picture url and sends to backend for saving
+ function readURL(input,pictureType) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            if (pictureType == 'Thumbnail') {
+                reader.onload = function (e) {
+                    $('#previewThumbnail')
+                        .attr('src', e.target.result);
+                }
+            }
+            else {
+            reader.onload = function (e) {
+                    $('#previewProfilePic')
+                        .attr('src', e.target.result);
+                }
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
