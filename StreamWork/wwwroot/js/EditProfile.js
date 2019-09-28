ed6a39a5de08f736c8c7254900a5f97878b10b51
rarun321 <43283635+rarun321@users.noreply.github.com>
2019-09-27 23:45:50 -0700

    Added Logout

--- StreamWork/wwwroot/js/EditProfile.js
+++ StreamWork/wwwroot/js/EditProfile.js
@@ -76,3 +76,19 @@ function Dashboard(profileType) {
          window.location.href = "/Student/ProfileStudent"
     }
 }
+
+function Logout() {
+    $.ajax({
+        url: '/Home/Logout',
+        type: 'post',
+        dataType: 'json',
+        data: {
+            'Logout': 'logout'
+        },
+        success: function (data) {
+            if (data.message === "Success") {
+                window.location.href = "/Home/Logout"
+            }
+        }
+    })
+}
