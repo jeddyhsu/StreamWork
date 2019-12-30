e43714cc3ae7ac96b9a623af8188d8b4d4c18972
rarun321 <43283635+rarun321@users.noreply.github.com>
2019-12-30 14:32:48 -0800

    Made Stream Apperance Changes

--- StreamWork/Controllers/StreamViewsController.cs
+++ StreamWork/Controllers/StreamViewsController.cs
@@ -16,19 +16,19 @@ namespace StreamWork.Controllers
         {
             return View();
         }
-        public async Task<IActionResult> StreamPage([FromServices] IOptionsSnapshot<StorageConfig> storageConfig, string streamKeyandchatId, string tutor)
+        public async Task<IActionResult> StreamPage([FromServices] IOptionsSnapshot<StorageConfig> storageConfig, string streamKeyandchatId)
         {
             if (HttpContext.User.Identity.IsAuthenticated == false)
                 return Redirect(_homehelperFunctions._host + "/Home/Login?dest=-StreamViews-StreamPage");
 
             var split = streamKeyandchatId.Split(new char[] { '|' });
             var secretChatKey = _homehelperFunctions.GetChatSecretKey(split[1], split[2], User.Identity.Name);
-            string[] arr = { split[0], secretChatKey };
+            string[] arr = { split[0], secretChatKey, split[4] };
 
             StreamPageViewModel model = new StreamPageViewModel {
                 profile = new ProfileTutorViewModel {
                     userProfile = User.Identity.Name != null ? await _homehelperFunctions.GetUserProfile(storageConfig, QueryHeaders.CurrentUser, User.Identity.Name) : null,
-                    studentOrtutorProfile = await _homehelperFunctions.GetUserProfile(storageConfig, QueryHeaders.CurrentUser, tutor)
+                    studentOrtutorProfile = await _homehelperFunctions.GetUserProfile(storageConfig, QueryHeaders.CurrentUser, split[3])
                 },
                 urlParams = arr
             };
@@ -36,15 +36,19 @@ namespace StreamWork.Controllers
             return View ("StreamPage", model);
         }
 
-        public async Task<IActionResult> StreamPlaybackPage([FromServices] IOptionsSnapshot<StorageConfig> storageConfig, string streamId)
+        public async Task<IActionResult> StreamPlaybackPage([FromServices] IOptionsSnapshot<StorageConfig> storageConfig, string streamInfo)
         {
             if (HttpContext.User.Identity.IsAuthenticated == false)
                 return Redirect(_homehelperFunctions._host + "/Home/Login?dest=-StreamViews-StreamPlaybackPage");
 
-            string[] arr = { streamId };
+            var split = streamInfo.Split("|");
+
+            string[] arr = { split[0], split[1] };
             StreamPageViewModel model = new StreamPageViewModel {
                 profile = new ProfileTutorViewModel {
-                    userProfile = User.Identity.Name != null ? await _homehelperFunctions.GetUserProfile(storageConfig, QueryHeaders.CurrentUser, User.Identity.Name) : null
+                    userProfile = User.Identity.Name != null ? await _homehelperFunctions.GetUserProfile(storageConfig, QueryHeaders.CurrentUser, User.Identity.Name) : null,
+                    numberOfStreams = (await _homehelperFunctions.GetArchivedStreams(storageConfig, QueryHeaders.UserArchivedVideos, User.Identity.Name)).Count
+
                 },
                 urlParams = arr
             };
