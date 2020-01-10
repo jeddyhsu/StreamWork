224ef49863eeb42eee20764d26a68ac4766a5c89
rarun321 <43283635+rarun321@users.noreply.github.com>
2020-01-10 11:35:52 -0800

    Revert "Revert "Bug Fixes""
    This reverts commit 5aab2d5f6e9a19021cef47ee2c953ab121dbfade.

--- StreamWork/ViewModels/ProfileTutorViewModel.cs
+++ StreamWork/ViewModels/ProfileTutorViewModel.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using StreamWork.DataModels;
 using StreamWork.TutorObjects;
 
--- StreamWork/ViewModels/StreamPageViewModel.cs
+++ StreamWork/ViewModels/StreamPageViewModel.cs
@@ -4,13 +4,13 @@ using StreamWork.DataModels;
 namespace StreamWork.ViewModels {
     public class StreamPageViewModel {
 
-        public UserLogin userProfile { get; set; }
-        public ProfileTutorViewModel profile { get; set; }
-        public string[] urlParams { get; set; }
-        public string streamName { get; set; }
+        public UserLogin UserProfile { get; set; }
+        public ProfileTutorViewModel Profile { get; set; }
+        public string[] UrlParams { get; set; }
+        public string StreamName { get; set; }
 
         public bool IsSubscribed () {
-            if (DateTime.UtcNow.CompareTo(profile.UserProfile.Expiration) < 0 || (profile.UserProfile.ProfileType.Equals("tutor") && profile.UserProfile.AcceptedTutor)) {
+            if (DateTime.UtcNow.CompareTo(Profile.UserProfile.Expiration) < 0 || (Profile.UserProfile.ProfileType.Equals("tutor") && Profile.UserProfile.AcceptedTutor)) {
                 return true;
             }
             return false;
