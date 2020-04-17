5d5eb3e743dab31de4756432db5748848022f674
rarun321 <43283635+rarun321@users.noreply.github.com>
2020-04-17 16:01:42 -0700

    Added dividers and changed university

--- StreamWork/Startup.cs
+++ StreamWork/Startup.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.AspNetCore.Authentication.Cookies;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
