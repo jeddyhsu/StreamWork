87cb419b6a2140dcd2c102966dab0dad0a616f16
rarun321 <43283635+rarun321@users.noreply.github.com>
2020-05-13 11:32:46 -0700

    Added Chat Functions

--- StreamWork/Hubs/ChatHub.cs
+++ StreamWork/Hubs/ChatHub.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 
@@ -6,9 +7,23 @@ namespace StreamWork.Hubs
 {
     public class ChatHub : Hub
     {
+        private static Hashtable idTable = new Hashtable();
+
+        public void RegisterConnection(string userId)
+        {
+            if (!idTable.Contains(userId)) idTable[userId] = Context.ConnectionId;
+            else idTable.Add(userId, Context.ConnectionId);
+        }
+
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task SendMessageToTutor(string studentName, string tutorId, string message)
+        {
+            string tutorConnectionId = (string)idTable[tutorId];
+            await Clients.User(tutorConnectionId).SendAsync("ReceiveMessage", studentName, message);
         }
     }
 }
--- StreamWork/wwwroot/js/Chat.js
+++ StreamWork/wwwroot/js/Chat.js
@@ -1,1 +1,32 @@
 ﻿var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+//Disable send button until connection is established
+
+
+//connection.on("ReceiveMessage", function (user, message) {
+//    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+//    var encodedMsg = user + " says " + msg;
+//    var li = document.createElement("li");
+//    li.textContent = encodedMsg;
+//    document.getElementById("messagesList").appendChild(li);
+//});
+
+
+function StartConnectionToServer(tutorId) {
+    connection.start().then(function () {
+        connection.invoke("RegisterConnection", tutorId).catch(function (err) {
+            return console.error(err.toString());
+        });
+    }).catch(function (err) {
+        return console.error(err.toString());
+    });
+}
+
+function SendMessage() {
+    var user = document.getElementById("userInput").textContent;
+    var message = document.getElementById("latex").textContent;
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+}
