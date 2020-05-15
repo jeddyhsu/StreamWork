eaca5113d7670d1f97aa1338d9a50d5991aca6e9
rarun321 <43283635+rarun321@users.noreply.github.com>
2020-05-15 10:03:41 -0700

    Added Chat Model

---
+++ StreamWork/DataModels/Chats.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace StreamWork.DataModels
+{
+    public class Chats
+    {
+        [Key]
+        public string Id { get; set; }
+        public string UserId { get; set; }
+        public string Name { get; set; }
+        public string Message { get; set; }
+        public DateTime Date { get; set; }
+        public string ProfilePicture { get; set; }
+
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+
+        public virtual void Configure(EntityTypeBuilder<View> builder)
+        {
+
+        }
+
+    }
+}
