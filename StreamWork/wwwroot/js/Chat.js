5ae7938da60fdcd1ffa3619a7638ad57c0995e7c
rarun321 <43283635+rarun321@users.noreply.github.com>
2020-05-28 10:36:51 -0700

    Audio fixes

--- StreamWork/wwwroot/js/Chat.js
+++ StreamWork/wwwroot/js/Chat.js
@@ -33,6 +33,13 @@ function PlayAudio() {
     var audioElement = document.createElement('audio');
     audioElement.setAttribute('src', '/media/juntos.mp3')
     var promise = audioElement.play();
+    promise.then(function () {
+        document.getElementById("sound").src = '/images/ChatAssets/Unmute.png';
+        muted = false;
+
+    }).catch(function () {
+        return document.getElementById("sound").src = '/images/ChatAssets/Mute.png';
+    })
 }
 
 function ToggleMuteAndUnmute() {
