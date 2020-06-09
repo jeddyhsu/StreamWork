e559d340303b15777e412b62d50d2a6bd3ad8eb6
rarun321 <43283635+rarun321@users.noreply.github.com>
2020-06-09 10:35:41 -0700

    Update EditProfileMethods.cs

--- StreamWork/HelperMethods/EditProfileMethods.cs
+++ StreamWork/HelperMethods/EditProfileMethods.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using StreamWork.Config;
 using StreamWork.Core;
-using StreamWork.DataModels;
 
 namespace StreamWork.HelperMethods
 {
