ab19221b29fcb862340142fa5a5dc06484aa53e4
rarun321 <43283635+rarun321@users.noreply.github.com>
2020-06-09 22:34:49 -0700

    Slider Bugs

--- StreamWork/wwwroot/js/TutorJS/TutorDashboard.js
+++ StreamWork/wwwroot/js/TutorJS/TutorDashboard.js
@@ -1,19 +1,19 @@
 ﻿function SliderProfile() {
-    $('#profileTab').tab('show');
+    $('#profile-tab').tab('show');
     $('#sliderObject').css("transform", "translate3d(10px, 0px, 0px)")
 }
 
 function SliderSchedule() {
-    $('#scheduleTab').tab('show');
+    $('#schedule-tab').tab('show');
     $('#sliderObject').css("transform", "translate3d(100px, 0px, 0px)")
 }
 
 function SliderStream() {
-    $('#streamTab').tab('show');
+    $('#stream-tab').tab('show');
     $('#sliderObject').css("transform", "translate3d(200px, 0px, 0px)")
 }
 
 function SliderComment() {
-    $('#commentTab').tab('show');
+    $('#comment-tab').tab('show');
     $('#sliderObject').css("transform", "translate3d(300px, 0px, 0px)")
 }
