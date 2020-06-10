70dc4c12375d531d6a9ab3615085355f18a1c293
rarun321 <43283635+rarun321@users.noreply.github.com>
2020-06-10 16:05:51 -0700

    Dashboard changes

--- StreamWork/wwwroot/js/TutorJS/TutorDashboard.js
+++ StreamWork/wwwroot/js/TutorJS/TutorDashboard.js
@@ -1,19 +1,19 @@
 ﻿function SliderProfile() {
     $('#profile-tab').tab('show');
-    $('#sliderObject').css("transform", "translate3d(10px, 0px, 0px)")
+    $('#sliderObject').css("transform", "translate3d(15px, 0px, 0px)")
 }
 
 function SliderSchedule() {
     $('#schedule-tab').tab('show');
-    $('#sliderObject').css("transform", "translate3d(100px, 0px, 0px)")
+    $('#sliderObject').css("transform", "translate3d(110px, 0px, 0px)")
 }
 
 function SliderStream() {
     $('#stream-tab').tab('show');
-    $('#sliderObject').css("transform", "translate3d(200px, 0px, 0px)")
+    $('#sliderObject').css("transform", "translate3d(210px, 0px, 0px)")
 }
 
 function SliderComment() {
     $('#comment-tab').tab('show');
-    $('#sliderObject').css("transform", "translate3d(300px, 0px, 0px)")
+    $('#sliderObject').css("transform", "translate3d(315px, 0px, 0px)")
 }
