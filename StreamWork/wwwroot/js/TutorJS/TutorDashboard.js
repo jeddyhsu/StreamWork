a7136680dbfb2703d2a02e91b7a070413d078a1d
rarun321 <43283635+rarun321@users.noreply.github.com>
2020-06-10 17:32:04 -0700

    Basic Info

--- StreamWork/wwwroot/js/TutorJS/TutorDashboard.js
+++ StreamWork/wwwroot/js/TutorJS/TutorDashboard.js
@@ -17,3 +17,18 @@ function SliderComment() {
     $('#comment-tab').tab('show');
     $('#sliderObject').css("transform", "translate3d(315px, 0px, 0px)")
 }
+
+function AddSection(event) {
+    event.preventDefault();
+    var section = "<div class='divider'></div><div id='formfrom' class='form-group col-lg-12'><label class='formHeaders'>Section 2 Title</label> <input class='form-control border rounded-0 formInput' placeholder='Title of section two!'><label class='formHeaders pt-3'>Description</label><textarea class='form-control border rounded - 0 formTextArea' placeholder='Tell us what you're studying, concentrations, passions, and other extra curricular activities here!'></textarea></div>"
+    document.getElementById("formRow").innerHTML += section
+    var e = document.getElementById("formfrom");
+    e.scrollIntoView();
+}
+
+
+    
+   
+        
+        
+                                       
