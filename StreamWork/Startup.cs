85eca292e26a1808aac4f33c4459160fd916ff62
rarun321 <43283635+rarun321@users.noreply.github.com>
2020-06-26 19:45:47 -0700

    Tom you need this in startup

--- StreamWork/Startup.cs
+++ StreamWork/Startup.cs
@@ -56,7 +56,7 @@ namespace StreamWork
             services.AddTransient<EditService>();
             services.AddTransient<CommentService>();
 
-            services.AddRazorPages();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
