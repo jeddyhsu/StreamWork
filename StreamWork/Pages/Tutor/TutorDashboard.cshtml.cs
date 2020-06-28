d52eeb0812db6d0ecb5159299357a691d74d2c6e
rarun321 <43283635+rarun321@users.noreply.github.com>
2020-06-27 17:02:41 -0700

    Refactor Tutor Dashboard & Tutor Profile Page

--- StreamWork/Pages/Tutor/TutorDashboard.cshtml.cs
+++ StreamWork/Pages/Tutor/TutorDashboard.cshtml.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Options;
 using StreamWork.DataModels;
 using StreamWork.HelperMethods;
 using StreamWork.Services;
