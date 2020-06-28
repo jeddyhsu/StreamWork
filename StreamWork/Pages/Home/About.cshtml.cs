9f39df5285f984de77cf29bf64230581a75ea18f
Seldom <ssaguUUUU@gmail.com>
2020-06-27 18:07:13 -0700

    About Page

---
+++ StreamWork/Pages/Home/About.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using StreamWork.DataModels;
+using StreamWork.Services;
+
+namespace StreamWork.Pages.Home
+{
+    public class AboutModel : PageModel
+    {
+        private readonly SessionService session;
+
+        public UserLogin GenericUserProfile { get; set; }
+
+        public AboutModel(SessionService session)
+        {
+            this.session = session;
+        }
+
+        public async void OnGet()
+        {
+            GenericUserProfile = await session.GetCurrentUser();
+        }
+    }
+}
