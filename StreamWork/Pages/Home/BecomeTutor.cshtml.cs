2c305f17ed990e79005b1fb0a7b4e512b32aee2f
Seldom <ssaguUUUU@gmail.com>
2020-06-27 18:00:28 -0700

    BecomeTutor (Still needs cleanup)

---
+++ StreamWork/Pages/Home/BecomeTutor.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using StreamWork.DataModels;
+using StreamWork.HelperMethods;
+using StreamWork.Services;
+
+namespace StreamWork.Pages.Home
+{
+    public class BecomeTutorModel : PageModel
+    {
+        private readonly SessionService session;
+        private readonly StorageService storage;
+
+        public UserLogin GenericUserProfile { get; set; }
+        public List<UserLogin> StreamTutors { get; set; }
+
+        public BecomeTutorModel(SessionService session, StorageService storage)
+        {
+            this.session = session;
+            this.storage = storage;
+        }
+
+        public async Task OnGet()
+        {
+            GenericUserProfile = await session.GetCurrentUser();
+            StreamTutors = await storage.GetList<UserLogin>(SQLQueries.GetAllApprovedTutors);
+        }
+    }
+}
