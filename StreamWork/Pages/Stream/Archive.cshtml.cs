7fb017a6a47ccb4cc36faee6da026d9a1bfbf1ca
rarun321 <43283635+rarun321@users.noreply.github.com>
2020-06-30 15:53:22 -0700

    Added Archive Page

---
+++ StreamWork/Pages/Stream/Archive.cshtml.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using StreamWork.DataModels;
+using StreamWork.HelperMethods;
+using StreamWork.Services;
+using StreamWork.TutorObjects;
+
+namespace StreamWork.Pages.Stream
+{
+    public class Archive : PageModel
+    {
+        private readonly SessionService sessionService;
+        private readonly StorageService storageService;
+        private readonly ProfileService profileService;
+        private readonly ScheduleService scheduleService;
+        private readonly FollowService followService;
+        private readonly EditService editService;
+        private readonly ChatService chatService;
+
+        public UserLogin UserProfile { get; set; }
+        public UserLogin TutorUserProfile { get; set; }
+        public UserChannel UserChannel { get; set; }
+        public UserArchivedStreams ArchivedStream { get; set; }
+        public string ChatInfo { get; set; }
+        public string StreamSubjectPicture { get; set; }
+        public string FollowValue { get; set; }
+        public List<UserArchivedStreams> UserArchivedStreams { get; set; }
+        public List<UserArchivedStreams> OtherArchivedStreams { get; set; }
+        public List<UserLogin> RelatedTutors { get; set; }
+        public List<Section> Sections { get; set; }
+        public List<Schedule> Schedule { get; set; }
+        public int NumberOfStreams { get; set; }
+        public int NumberOfFollowers { get; set; }
+        public int NumberOfViews { get; set; }
+
+        public Archive(StorageService storage, SessionService session, ProfileService profile, ScheduleService schedule, FollowService follow, EditService edit, ChatService chat)
+        {
+            storageService = storage;
+            sessionService = session;
+            profileService = profile;
+            scheduleService = schedule;
+            followService = follow;
+            editService = edit;
+            chatService = chat;
+        }
+
+        public async Task<IActionResult> OnGet(string tutor, string id)
+        {
+            UserProfile = await sessionService.GetCurrentUser();
+            TutorUserProfile = await storageService.Get<UserLogin>(SQLQueries.GetUserWithUsername, tutor);
+            UserChannel = await storageService.Get<UserChannel>(SQLQueries.GetUserChannelWithUsername, tutor);
+            ArchivedStream = await storageService.Get<UserArchivedStreams>(SQLQueries.GetArchivedStreamsWithStreamId, id);
+            ChatInfo = "1234";
+            FollowValue = await followService.IsFollowingFollowee(UserProfile.Id, TutorUserProfile.Id);
+
+            UserArchivedStreams = await storageService.GetList<UserArchivedStreams>(SQLQueries.GetArchivedStreamsWithUsername, new string[] { TutorUserProfile.Username });
+            OtherArchivedStreams = await storageService.GetList<UserArchivedStreams>(SQLQueries.GetAllArchivedStreams, new string[] { });
+            RelatedTutors = (await storageService.GetList<UserLogin>(SQLQueries.GetAllTutorsNotInTheList, new string[] { TutorUserProfile.Id })).GetRange(0, 5);
+            Sections = profileService.GetSections(TutorUserProfile);
+            Schedule = await scheduleService.GetSchedule(UserProfile.Username);
+
+            NumberOfStreams = UserArchivedStreams.Count;
+            NumberOfFollowers = await followService.GetNumberOfFollowers(UserProfile.Id);
+            NumberOfViews = UserArchivedStreams.Sum(x => x.Views);
+
+            return Page();
+        }
+    }
+}
