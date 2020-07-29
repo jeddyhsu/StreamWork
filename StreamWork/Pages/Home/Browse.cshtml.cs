34781db798cc5c13524520107fd2ae49a45e9f52
rarun321 <43283635+rarun321@users.noreply.github.com>
2020-07-29 10:28:09 -0700

    Added Following Page Student Dash

---
+++ StreamWork/Pages/Home/Browse.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using StreamWork.DataModels;
+using StreamWork.Services;
+
+namespace StreamWork.Pages.Home
+{
+    public class BrowseModel : PageModel
+    {
+        public UserLogin CurrentUserProfile { get; set; }
+
+        private readonly CookieService cookieService;
+
+        public BrowseModel(CookieService cookie)
+        {
+            cookieService = cookie;
+        }
+
+        public IActionResult OnGet()
+        {
+            //CurrentUserProfile 
+            return Page();
+        }
+    }
+}
---
+++ StreamWork/ProfileObjects/FollowedTutors.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using StreamWork.DataModels;
+
+namespace StreamWork.ProfileObjects
+{
+    public class FollowedTutors
+    {
+        public UserLogin Tutor;
+        public List<UserArchivedStreams> PreviousStreams;
+        public Schedule LatestScheduledStream;
+        public string FollowValue;
+
+        public FollowedTutors(UserLogin Tutor, List<UserArchivedStreams> PreviousStreams, Schedule LatestScheduledStream, string FollowValue)
+        {
+            this.Tutor = Tutor;
+            this.PreviousStreams = PreviousStreams;
+            this.LatestScheduledStream = LatestScheduledStream;
+            this.FollowValue = FollowValue;
+        }
+    }
+}
