10d86ed34bef5120c9afc918d74286f9ccc73cc6
Seldom <ssaguUUUU@gmail.com>
2020-08-09 16:53:46 -0700

    EmailService (needs authentication)

--- StreamWork/Startup.cs
+++ StreamWork/Startup.cs
@@ -71,7 +71,9 @@ namespace StreamWork
             services.AddTransient<StorageService>(); // Transient means it creates a new instance every time it's needed
             services.AddTransient<StreamService>(); // You should use Transient by default
             services.AddTransient<TopicService>();
-            services.AddSingleton<ImageService>(); // Singleton creates a shared instance the first time it's needed
+            services.AddSingleton<EmailService>(); // Singleton creates a shared instance the first time it's needed
+            services.AddSingleton<ImageService>(); // You should use them if their constructor builds a lot of data ex. a hashmap
+            // Singletons can't use transient services, so be careful about how you structure your services
 
             services.AddAntiforgery(o => o.HeaderName = "XSRF-TOKEN");
 
